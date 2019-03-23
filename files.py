6559ef476eb9936f2ad419a8fd1d72039053ec54
StoneY1 <35155689+StoneY1@users.noreply.github.com>
2019-03-23 02:43:57 -0400

    fixed indexing and yield mistake in files.get_from_dir()

--- files.py
+++ files.py
@@ -2,6 +2,6 @@ import os
 
 def get_from_dir(input_dir):
         for file in os.listdir(input_dir):
-            file_name = file.split('.')
+            file_name = file.split('.')[0]
         
-        yield file, file_name
+            yield file, file_name
