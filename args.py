5edd653bebd531631c71975131b1399982e14a59
StoneY1 <35155689+StoneY1@users.noreply.github.com>
2019-03-23 01:09:26 -0400

    greated argument parser helpers

---
+++ args.py
@@ -0,0 +1,16 @@
+import argparse
+parser = argparse.ArgumentParser
+
+def add_arg(shortform, longform, required=False):
+    """use for adding single argument"""
+    parser.add_argument(shortform, longform, required=required)
+
+def add_args(*args):
+    """use for adding multiple arguments, short form and long form need to be grouped together"""
+    num_args = len(args)
+    for i in range(0,num_args,2):
+        add_arg(args[i], args[i+1])
+
+def get_arg(arg_name):
+    args = vars(parser.parse_args())
+    return args[arg_name]
