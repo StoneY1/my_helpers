ce5dd68f927b9da48e62633c9045bd36a2eb167c
StoneY1 <35155689+StoneY1@users.noreply.github.com>
2019-03-23 02:34:02 -0400

    added __init__.py for explicit package import

--- files.py
+++ files.py
@@ -2,6 +2,6 @@ import os
 
 def get_from_dir(input_dir):
         for file in os.listdir(input_dir):
-        file_name = file.split('.')
+            file_name = file.split('.')
         
         yield file, file_name
